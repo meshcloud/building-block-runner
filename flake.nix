935db359e6ad76161ad8c61cfcd5fdddb896685b
Mohammad Alhussan <malhussan@meshcloud.io>
2026-05-19 13:41:57 +0200

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix flake for the building-block-runner developer shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        shell_hook =
+          if pkgs.stdenv.isDarwin then ''
+            # getting rid of "warning: unhandled Platform key FamilyDisplayName" on macOS
+            unset DEVELOPER_DIR
+          '' else "";
+
+        core_packages = [
+          pkgs.go
+          pkgs.golangci-lint
+          pkgs.jdk21_headless
+          pkgs.opentofu
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "building-block-runner shell";
+          packages = core_packages;
+          hardeningDisable = [ "fortify" ]; # to be able to debug golang, c.f. https://nixos.wiki/wiki/Go
+          shellHook = shell_hook;
+        };
+      }
+    );
+}
